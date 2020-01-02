f1657307814423afeb9914052c652f1488e75678
maikeurios <maikesystem@gmail.com>
2020-01-02 19:11:11 -0300

    commit

--- secindex.php
+++ secindex.php
@@ -26,63 +26,69 @@ include_once 'includes/message.php';
 
             </thead>
 
+            <div class="input-field col s12">
+                <input type="text" name="nome" id="nome">
+                <label for="dia">Digite o nome do palestrante para consulta</label>
+            </div>
+           
             <tbody>
                 <?php
 				$sql = "SELECT * FROM palestrantes ORDER BY palestrante ASC";
 				$resultado = mysqli_query($connect, $sql);
                
                 if(mysqli_num_rows($resultado) > 0):
+                
 
 				while($dados = mysqli_fetch_array($resultado)):
 				?>
-                <tr>
-                    <td><?php echo $dados['palestrante']; ?></td>
-                    <td><?php echo $dados['dia']; ?></td>
-                    <td><?php echo $dados['sala']; ?></td>
-                    <td><?php echo $dados['sessao']; ?></td>
-                    <td><?php echo $dados['ordem']; ?></td>
-                    <td><?php echo $dados['caminho']; ?></td>
+            <tr>
+                <td><?php echo $dados['palestrante']; ?></td>
+                <td><?php echo $dados['dia']; ?></td>
+                <td><?php echo $dados['sala']; ?></td>
+                <td><?php echo $dados['sessao']; ?></td>
+                <td><?php echo $dados['ordem']; ?></td>
+                <td><?php echo $dados['caminho']; ?></td>
 
-                    <!--<td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+
+                <!--<td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
 
 					<td><a href="#modal<?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
 
 					<td><a href="#moda<?php echo $dados['id']; ?>" class="btn-floating red"><i class="material-icons">C</i></a></td>-->
 
-                    <!-- Modal Structure -->
-                    <div id="modal<?php echo $dados['id']; ?>" class="modal">
-                        <div class="modal-content">
-                            <h4>Opa!</h4>
-                            <p>Tem certeza que deseja excluir esse palestrante?</p>
-                        </div>
-                        <div class="modal-footer">
-
-                            <form action="php_action/delete.php" method="POST">
-                                <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
-                                <button type="submit" name="btn-deletar" class="btn red">Sim, quero deletar</button>
-
-                                <a href="#!"
-                                    class="modal-action modal-close waves-effect waves-green btn-flat">Cancelar</a>
-
-                            </form>
-
-                        </div>
+                <!-- Modal Structure -->
+                <div id="modal<?php echo $dados['id']; ?>" class="modal">
+                    <div class="modal-content">
+                        <h4>Opa!</h4>
+                        <p>Tem certeza que deseja excluir esse palestrante?</p>
                     </div>
+                    <div class="modal-footer">
 
-                </tr>
-                <?php 
+                        <form action="php_action/delete.php" method="POST">
+                            <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
+                            <button type="submit" name="btn-deletar" class="btn red">Sim, quero deletar</button>
+
+                            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cancelar</a>
+
+                        </form>
+
+                    </div>
+                </div>
+
+            </tr>
+            <?php 
 				endwhile;
 				else: ?>
 
-                <tr>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
+            <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
 
-                <?php 
+            <?php 
 				endif;
 			   ?>
 
